ee99ec15f72fb7c78a8de1b7c9bff9c964943b5c
jozz024 <74272560+jozz024@users.noreply.github.com>
2021-09-12 11:08:42 -0500

    fix code, requirements.txt
    got rid if unnecessary import, message send channel is still messed up

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import requests
 import discord
-import keep_alive
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 TOKEN = os.environ['TOKEN']
 AMIIBOID = os.environ['AMIIBO-ID']
@@ -23,21 +23,20 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         if USER in message.content:
-            message_send_channel = client.get_channel(PINGCHANNEL)
             url = f'https://www.amiibots.com/api/amiibo/{AMIIBOID}'
             headers = {'Authorization': APIKEY}
-            payload = {'is_active': False}
-            response = requests.put(url, headers=headers, json=payload)
-            response1 = requests.get(url, headers=headers)
-            match_turn_off_numbers = [29, 40, 49, 99, 149]
-            amiibo = response1.json()['data']
+            amiibodata = requests.get(url, headers=headers)
+            match_turn_off_numbers = [29, 49, 99, 149]
+            amiibo = amiibodata.json()['data']
             if amiibo['total_matches'] in match_turn_off_numbers:
-                print(message_send_channel)
+                url = f'https://www.amiibots.com/api/amiibo/{AMIIBOID}'
+                headers = {'Authorization': APIKEY}
+                payload = {'is_active': False}
+                response = requests.put(url, headers=headers, json = payload)
+                message_send_channel = client.get_channel(PINGCHANNEL)
                 await message_send_channel.send(
-                    f"<@{USERID}> Your amiibo, {amiibo['name']}, finished its run with {amiibo['total_matches'] + 1} matches!\n {response.json} "
+                    f"<@{USERID}> Your amiibo, {amiibo['name']}, finished its run with {amiibo['total_matches'] + 1} matches!\n {response.text}"
                 )
 
-
-keep_alive.keep_alive()
 client = MyClient()
 client.run(TOKEN)
