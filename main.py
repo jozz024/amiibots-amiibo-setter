49f4d2c72f6241d52eec06a9f9217e29a62a2b9a
jozz024 <74272560+jozz024@users.noreply.github.com>
2021-09-12 12:15:33 -0500

    Improvements and channel fix
    fully functional once again pog

--- main.py
+++ main.py
@@ -5,37 +5,37 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-TOKEN = os.environ['TOKEN']
+TOKEN = os.environ['BOT-TOKEN']
 AMIIBOID = os.environ['AMIIBO-ID']
 APIKEY = os.environ['AMIIBOTS-API-KEY']
 USERID = os.environ['USERID']
 USER = os.environ['USERNAME']
-PINGCHANNEL = os.environ['PINGCHANNEL']
+PINGCHANNEL = int(os.environ['PINGCHANNEL'])
+SHOUTBOXID = int(os.environ['SHOUTBOXID'])
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
         activity = discord.Game(
             name=f'Pinging {USER} when their amiibo finish their amiibots run!')
-        await client.change_presence(status=discord.Status.idle,
-                                     activity=activity)
-
+        await client.change_presence(status=discord.Status.idle, activity=activity)
+    url = f'https://www.amiibots.com/api/amiibo/{AMIIBOID}'
+    headers = {'Authorization': APIKEY}
+    amiibodata = requests.get(url, headers=headers)
+    match_turn_off_numbers = [29, 49, 99, 149]
+    amiibo = amiibodata.json()['data']
     async def on_message(self, message):
-        if USER in message.content:
-            url = f'https://www.amiibots.com/api/amiibo/{AMIIBOID}'
-            headers = {'Authorization': APIKEY}
-            amiibodata = requests.get(url, headers=headers)
-            match_turn_off_numbers = [29, 49, 99, 149]
-            amiibo = amiibodata.json()['data']
-            if amiibo['total_matches'] in match_turn_off_numbers:
+        if message.author == client.user:
+            return
+        if USERID in message.content and message.channel.id == SHOUTBOXID:
+            if MyClient.amiibo['total_matches'] in MyClient.match_turn_off_numbers:
                 url = f'https://www.amiibots.com/api/amiibo/{AMIIBOID}'
                 headers = {'Authorization': APIKEY}
                 payload = {'is_active': False}
                 response = requests.put(url, headers=headers, json = payload)
                 message_send_channel = client.get_channel(PINGCHANNEL)
                 await message_send_channel.send(
-                    f"<@{USERID}> Your amiibo, {amiibo['name']}, finished its run with {amiibo['total_matches'] + 1} matches!\n {response.text}"
+                    f"<@{USERID}> Your amiibo, {MyClient.amiibo['name']}, finished its run with {MyClient.amiibo['total_matches'] + 1} matches!"
                 )
 
 client = MyClient()
