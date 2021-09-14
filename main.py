9d717002de94570b91fe4942345a25e377d6858b
jozz024 <74272560+jozz024@users.noreply.github.com>
2021-09-14 15:38:55 -0500

    working^tm

--- main.py
+++ main.py
@@ -10,17 +10,20 @@ DISABLEAMIIBOID = os.environ['DISABLE-AMIIBO-ID']
 ENABLEAMIIBOID = os.environ['ENABLE-AMIIBO-ID']
 APIKEY = os.environ['AMIIBOTS-API-KEY']
 USERID = os.environ['USERID']
-USER = os.environ['USERNAME']
+USER = os.environ['TWITCH-USERNAME']
 PINGCHANNEL = int(os.environ['PINGCHANNEL'])
 SHOUTBOXID = int(os.environ['SHOUTBOXID'])
 
 BASE_URL = 'https://www.amiibots.com/api/amiibo/'
 OLD_AMIIBO_URL = BASE_URL+DISABLEAMIIBOID
 NEW_AMIIBO_URL = BASE_URL+ENABLEAMIIBOID
-HEADERS = {'Authorization': APIKEY}
+if 'Bearer' in APIKEY: 
+    HEADERS = {'Authorization': APIKEY}
+else:
+    HEADERS = {'Authorization': f'Bearer {APIKEY}'}
 MATCH_TURN_OFF_NUMBERS = [30, 50, 100, 150]
 amiibo_data_name = requests.get(OLD_AMIIBO_URL, headers=HEADERS)
-amiiboname = amiibo_data_name.json()['data']
+amiiboname = amiibo_data_name.json()['data']['name']
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -30,7 +33,7 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author == client.user:
             return
-        if (USERID in message.content or USER in message.content) and amiiboname['name'] in message.content and message.channel.id == SHOUTBOXID:
+        if (USERID in message.content or USER in message.content) and amiiboname in message.content and message.channel.id == SHOUTBOXID:
             amiibo_data = requests.get(OLD_AMIIBO_URL, headers=HEADERS)
             amiibo = amiibo_data.json()['data']
             if amiibo['total_matches'] + 1 in MATCH_TURN_OFF_NUMBERS:
