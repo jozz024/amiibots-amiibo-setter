0693a128ccd8a1c34e67101b631c093f81d7f543
jozz024 <74272560+jozz024@users.noreply.github.com>
2021-09-13 20:48:05 -0500

    nfp
    fix stuff, improve code, still no readme

--- main.py
+++ main.py
@@ -14,32 +14,35 @@ USER = os.environ['USERNAME']
 PINGCHANNEL = int(os.environ['PINGCHANNEL'])
 SHOUTBOXID = int(os.environ['SHOUTBOXID'])
 
+BASE_URL = 'https://www.amiibots.com/api/amiibo/'
+OLD_AMIIBO_URL = BASE_URL+DISABLEAMIIBOID
+NEW_AMIIBO_URL = BASE_URL+ENABLEAMIIBOID
+HEADERS = {'Authorization': APIKEY}
+MATCH_TURN_OFF_NUMBERS = [30, 50, 100, 150]
+amiibo_data_name = requests.get(OLD_AMIIBO_URL, headers=HEADERS)
+amiiboname = amiibo_data_name.json()['data']
+
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-        activity = discord.Game(
-            name=f'Pinging {USER} when their amiibo finish their amiibots run!')
+        print(f'Logged on as {self.user}!')
+        activity = discord.Game(name=f'Pinging {USER} when their amiibo finish their amiibots run!')
         await client.change_presence(status=discord.Status.idle, activity=activity)
-    url = f'https://www.amiibots.com/api/amiibo/{DISABLEAMIIBOID}'
-    headers = {'Authorization': APIKEY}
-    amiibodata = requests.get(url, headers=headers)
-    match_turn_off_numbers = [29, 49, 99, 149]
-    amiibo = amiibodata.json()['data']
     async def on_message(self, message):
         if message.author == client.user:
             return
-        if (USERID or USER) in message.content and message.channel.id == SHOUTBOXID:
-            if MyClient.amiibo['total_matches'] in MyClient.match_turn_off_numbers:
-                disableurl = f'https://www.amiibots.com/api/amiibo/{DISABLEAMIIBOID}'
-                headers = {'Authorization': APIKEY}
+        if (USERID in message.content or USER in message.content) and amiiboname['name'] in message.content and message.channel.id == SHOUTBOXID:
+            amiibo_data = requests.get(OLD_AMIIBO_URL, headers=HEADERS)
+            amiibo = amiibo_data.json()['data']
+            if amiibo['total_matches'] + 1 in MATCH_TURN_OFF_NUMBERS:
                 disablepayload = {'is_active': False}
-                disableamiibo = requests.put(disableurl, headers=headers, json = disablepayload)
+                disabled_amiibo = requests.put(OLD_AMIIBO_URL, headers=HEADERS, json=disablepayload)
                 message_send_channel = client.get_channel(PINGCHANNEL)
                 await message_send_channel.send(
-                    f"<@{USERID}> Your amiibo, {MyClient.amiibo['name']}, finished its run with {MyClient.amiibo['total_matches'] + 1} matches!")
-                enableurl = f'https://www.amiibots.com/api/amiibo/{ENABLEAMIIBOID}'
-                enablepayload = {'is_active': True}
-                enableamiibo = requests.put(enableurl, headers=headers, json = enablepayload)
+                    f"<@{USERID}> Your amiibo, {amiibo['name']}, finished its run with {amiibo['total_matches'] + 1} matches!")
+                if ENABLEAMIIBOID is not None:
+                    enablepayload = {'is_active': True}
+                    enableamiibo = requests.put(NEW_AMIIBO_URL, headers=HEADERS, json=enablepayload)
+
 
 client = MyClient()
 client.run(TOKEN)
